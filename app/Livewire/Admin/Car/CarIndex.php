22fdfd13d384bd79e7c7c9eb5bc175161a9c9001
maulana677 <maull0776@gmail.com>
2024-05-19 22:31:45 +0700

    fix: membuat function hapus data

--- app/Livewire/Admin/Car/CarIndex.php
+++ app/Livewire/Admin/Car/CarIndex.php
@@ -4,13 +4,21 @@ namespace App\Livewire\Admin\Car;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use App\Models\Car;
+
 
 class CarIndex extends Component
 {
+    public function delete($id)
+    {
+        Car::find($id)->delete();
+    }
 
     #[Layout('admin.layouts.master')]
     public function render()
     {
-        return view('livewire.admin.car.car-index');
+        return view('livewire.admin.car.car-index', [
+            'cars' => Car::all()
+        ]);
     }
 }
--- resources/views/livewire/admin/car/car-index.blade.php
+++ resources/views/livewire/admin/car/car-index.blade.php
@@ -8,80 +8,67 @@
 
     <div class="card border-0">
         <div class="card-body">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Foto</th>
-                            <th>Merek</th>
-                            <th>Model</th>
-                            <th>Warna</th>
-                            <th>Tahun Pembuatan</th>
-                            <th>Kapasitas Penumpang</th>
-                            <th>Harga</th>
-                            <th>Status</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="align-middle">
-                            <td>1</td>
-                            <td>
-                                <img src="{{ url('assets/images/cars/agya_lrg.jpg') }}" alt="Mobil 1"
-                                    class="rounded-md" style="width: 80px;">
-                            </td>
-                            <td>Agya</td>
-                            <td>Avanza</td>
-                            <td>Merah</td>
-                            <td>2020</td>
-                            <td>5 Kursi</td>
-                            <td>Rp. 760.000/hari</td>
-                            <td><span class="badge bg-primary">On Going</span></td>
-                            <td>
-                                <div class="dropdown">
-                                    <button class="btn btn-light rounded-circle p-2" type="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false"
-                                        style="width: 40px; height: 40px;">
-                                        <i class="bx bx-dots-horizontal"></i>
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Edit</a></li>
-                                        <li><a class="dropdown-item" href="#">Hapus</a></li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td>2</td>
-                            <td>
-                                <img src="{{ url('assets/images/cars/avanza_lrg.jpg') }}" alt="Mobil 1"
-                                    class="rounded-md" style="width: 80px;">
-                            </td>
-                            <td>Agya</td>
-                            <td>Avanza</td>
-                            <td>Merah</td>
-                            <td>2020</td>
-                            <td>5 Kursi</td>
-                            <td>Rp. 760.000/hari</td>
-                            <td><span class="badge bg-primary">On Going</span></td>
-                            <td>
-                                <div class="dropdown">
-                                    <button class="btn btn-light rounded-circle p-2" type="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false"
-                                        style="width: 40px; height: 40px;">
-                                        <i class="bx bx-dots-horizontal"></i>
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Edit</a></li>
-                                        <li><a class="dropdown-item" href="#">Hapus</a></li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            @if ($cars->count() > 0)
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Foto</th>
+                                <th>Merek</th>
+                                <th>Model</th>
+                                <th>Warna</th>
+                                <th>Tahun Pembuatan</th>
+                                <th>Kapasitas Penumpang</th>
+                                <th>Harga</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($cars as $item)
+                                <tr class="align-middle">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        <img src="{{ url('storage/' . $item->image) }}" alt="Mobil 1"
+                                            class="rounded-md" style="width: 80px;">
+                                    </td>
+                                    <td>{{ $item->brand }}</td>
+                                    <td>{{ $item->model }}</td>
+                                    <td>{{ $item->color }}</td>
+                                    <td>{{ $item->production_year }}</td>
+                                    <td>{{ $item->seats }} Kursi</td>
+                                    <td>Rp. {{ number_format($item->price) }}/hari</td>
+                                    <td>
+                                        @if ($item->status == 'Available')
+                                            <span class="badge bg-success">{{ $item->status }}</span>
+                                        @else
+                                            <span class="badge bg-primary">{{ $item->status }}</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-light rounded-circle p-2" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false"
+                                                style="width: 40px; height: 40px;">
+                                                <i class="bx bx-dots-horizontal"></i>
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Edit</a></li>
+                                                <li><a class="dropdown-item" href="#"
+                                                        wire:confirm="Apakah kamu yakin ingin menghapus data ini"
+                                                        wire:click="delete({{ $item->id }})">Hapus</a></li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <p class="mb-0 text-center text-danger">Tidak ada mobil</p>
+            @endif
         </div>
     </div>
 </div>
